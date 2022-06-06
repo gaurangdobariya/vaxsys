1d8dcc0d217ec0eca3722c48d158e394f2ea799f
Madhuvanthi Hemanathan <madhunarmi@gmail.com>
2022-06-06 19:34:29 -0400

    login body changes

--- src/main/java/com/soen6471/vaxsys/controller/AuthenticationController.java
+++ src/main/java/com/soen6471/vaxsys/controller/AuthenticationController.java
@@ -4,6 +4,7 @@ import com.soen6471.vaxsys.dto.LoginDto;
 import com.soen6471.vaxsys.dto.PatientDto;
 import com.soen6471.vaxsys.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -22,7 +23,7 @@ public class AuthenticationController {
      * @param patientDto This is for patient information
      * @return id of the user if email and password match else null in case of any error/invalidation
      */
-    @PostMapping("/register")
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
     public Integer register(@RequestBody PatientDto patientDto){
         System.out.println(patientDto);
         return authenticationService.register(patientDto);
@@ -31,15 +32,11 @@ public class AuthenticationController {
     /**
      *
      * This is used for login purpose by providing emailId and password.
-     * @param emailId This is for email ID field as a param
-     * @param password This is for password field as a param
+     * @param loginDto This is for email ID field as a param
      * @return id of the user if email and password match else null in case of any error/invalidation
      */
-    @PostMapping("/login")
-    public Integer login(@RequestParam String emailId,@RequestParam String password){
-        LoginDto loginDto = new LoginDto();
-        loginDto.setEmailId(emailId);
-        loginDto.setPassword(password);
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Integer login(@RequestBody LoginDto loginDto){
         return authenticationService.login(loginDto);
     }
 }
